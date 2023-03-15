4e6beb3df4006aac7a20700d6594ecf72d253c83
sigeer <1452069418@qq.com>
2023-03-15 17:11:12 +0800

    dropdown

--- WebTools/vue-components/v3/src/App.vue
+++ WebTools/vue-components/v3/src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 import FileUploadSelector from "./components/FileUploadSelector.vue";
 import EModal from "./components/EModal.vue";
 import WaitTag from "./components/WaitTag.vue";
 import ELoading from "./components/ELoading.vue";
 
+import Dropdown from "./components/Dropdown.vue";
+
 import Loading from "./directives/Loading/Loading";
 const vLoading = Loading;
+
+const isDisabled = ref(false);
+const toggleDisabled = () => {
+  isDisabled.value = !isDisabled.value;
+};
 
 const afterSelected = (evt) => {
   console.log(evt);
@@ -40,10 +47,44 @@ const loadingEvent = () => {
     isLoading.value = false;
   }, 1000);
 };
+
+const dropdown = reactive({
+  ds: [
+    { text: "111", value: 1 },
+    { text: "222", value: 2 },
+    { text: "333", value: 3 },
+    { text: "444", value: 4 },
+    { text: "555", value: 5 },
+    { text: "666", value: 6 },
+    { text: "777", value: 7 },
+    { text: "8881111111111111111111111111", value: 8 },
+  ],
+  ds1: [
+    { key: "key111", value: 1 },
+    { key: "key222", value: 2 },
+    { key: "key333", value: 3 },
+    { key: "key444", value: 4 },
+    { key: "key555", value: 5 },
+    { key: "key666", value: 6 },
+    { key: "key777", value: 7 },
+    { key: "key888", value: 8 },
+  ],
+});
+
+const dModel = ref(8);
+const dropDownChange = (evt) => {
+  console.log(evt);
+};
+const setdModel = () => {
+  dModel.value = (dModel.value + 1) % 8 || 8;
+};
 </script>
 
 <template>
   <div>
+    <div>
+      <button @click="toggleDisabled">toggle disable</button>
+    </div>
     <div>
       <label>UploadFile组件</label>
       <file-upload-selector
@@ -126,12 +167,72 @@ const loadingEvent = () => {
           style="border: 1px solid"
         >
           <template #loading-tips> 正在加载... </template>
-          <button @click="loadingEvent">eloading组件 隐藏原内容 隐藏默认图标</button>
+          <button @click="loadingEvent">
+            eloading组件 隐藏原内容 隐藏默认图标
+          </button>
         </e-loading>
       </div>
     </div>
 
     <hr />
-    <div></div>
+    <div>
+      简单用法：
+      <div>
+        <dropdown
+          style="width: 200px"
+          :disabled="isDisabled"
+          v-model="dModel"
+          :data-source="dropdown.ds"
+          @change="dropDownChange"
+        ></dropdown>
+      </div>
+      dModel.value: {{ dModel }}
+      <button @click="setdModel">修改dModel值</button>
+      <div>
+        进阶用法：
+        <div style="width: 200px">
+          <dropdown
+            place-holder="请选择"
+            :disabled="isDisabled"
+            :data-source="dropdown.ds"
+            @change="dropDownChange"
+            can-search
+          >
+            <template #item="x">
+              {{ x.item.text }} - {{ x.item.value }}
+            </template>
+          </dropdown>
+          11111111111111111111111111111111111111111111111111
+        </div>
+      </div>
+      <div>
+        自动计算direction，结合loading
+        <div>
+          <dropdown
+            v-model="dModel"
+            :disabled="isDisabled"
+            v-loading="isLoading"
+            :data-source="dropdown.ds1"
+            style="width: 200px"
+            text-prop-name="key"
+            @change="dropDownChange"
+            can-search
+          >
+            <template #display="x">
+              {{ x.item.key }} - {{ x.item.value }}
+            </template>
+            <template #item="x">
+              {{ x.item.key }} - {{ x.item.value }}
+            </template>
+          </dropdown>
+          11111111111111111111111111111111111111111111111111
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+
+      <hr />
+    </div>
   </div>
 </template>
