73884fdfd41097271894eb1752471aa16182258d
sigeer <1452069418@qq.com>
2023-04-25 09:11:11 +0800

    no message

--- PowerShellScripts/Others/RLOWording.ps1
+++ PowerShellScripts/Others/RLOWording.ps1
@@ -1,10 +1,14 @@
-Write-Host "Input..."
-$wording = Read-Host
-$wordingArray = $wording -split ""
-$result = ''
-for ($i = $wordingArray.Count - 1; $i -gt 0; $i--) {
-    <# Action that will repeat until the condition is met #>
-    $result = $result + $wordingArray[$i] + [char]0x202e
+while ($true) {
+    Write-Host "Input..."
+    $wording = Read-Host
+    $wordingArray = $wording -split ""
+    $result = ''
+    for ($i = $wordingArray.Count - 1; $i -gt 0; $i--) {
+        <# Action that will repeat until the condition is met #>
+        $result = $result + $wordingArray[$i] + [char]0x202e
+    }
+    $result += [char]0x202d
+    $result += [char]0x202d
+    Set-Clipboard -Value $result
+    Write-Host "Copied"
 }
-Set-Clipboard -Value $result
-Write-Host "Copied"
