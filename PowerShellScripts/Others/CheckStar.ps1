1cc5772526f471d337c432baa3d75cda7f0b56a5
sigeer <1452069418@qq.com>
2023-05-09 13:51:10 +0800

    更新：刷星脚本更新

--- PowerShellScripts/Others/CheckStar.ps1
+++ PowerShellScripts/Others/CheckStar.ps1
@@ -6,6 +6,7 @@
         "accept-language" = "zh-CN,zh;q=0.9"
         "accept-encoding" = "gzip, deflate, br"
         "content-type" = "application/json; charset=utf-8"
+        "referer"       = "https://ing.cnblogs.com/"
         "x-requested-with" = "XMLHttpRequest"
     }
     UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
