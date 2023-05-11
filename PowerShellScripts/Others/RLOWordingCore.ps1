5fd42ae3b6c21c50cf6d1930769f1c431c75bb91
sigeer <1452069418@qq.com>
2023-05-11 11:12:55 +0800

    unicode字符处理：部分表情实际上是2个字符

--- PowerShellScripts/Others/RLOWordingCore.ps1
+++ PowerShellScripts/Others/RLOWordingCore.ps1
@@ -3,12 +3,22 @@ param (
 )
 
 $wording = $Content
-$wordingArray = $wording -split ""
-$result = [char]0x202e 
-for ($i = $wordingArray.Count - 1; $i -gt 0; $i--) {
-    <# Action that will repeat until the condition is met #>
-    $result += $wordingArray[$i]
+$stringInfo = New-Object System.Globalization.StringInfo $wording 
+$wordingArray = for ($i = 0; $i -lt $stringInfo.LengthInTextElements; $i++) {
+    $stringInfo.SubstringByTextElements($i, 1)
 }
+
+$reverse = ""
+if ($wordingArray.Count -eq 1) {
+    $reverse = $wordingArray
+} else {
+    for ($i = $wordingArray.Count - 1; $i -ge 0; $i--) {
+        <# Action that will repeat until the condition is met #>
+        $reverse += $wordingArray[$i]
+    }
+}
+
+$result = [char]0x202e  + $reverse
 # 正文（原区域）恢复顺序
 $result += [char]0x202d
 # 系统文本恢复顺序
