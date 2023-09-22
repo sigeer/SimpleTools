6640bcb0faf850318c135cc11dae3aa0b5b0b32d
sigeer <1452069418@qq.com>
2023-09-22 14:40:51 +0800

    no message

--- WebTools/vue-components/v3/src/samples/EInputSample.vue
+++ WebTools/vue-components/v3/src/samples/EInputSample.vue
@@ -5,6 +5,13 @@ import EInput from '../components/input/EInput.vue';
 const s1 = ref('abc');
 
 const s2 = ref(0);
+
+const model = ref({
+    s1: 1
+})
+
+// s2非顶级不会自动解包
+const obj = { s2: ref(3) }
 </script>
 
 <template>
@@ -12,6 +19,7 @@ const s2 = ref(0);
         <div>
             普通用法： {{ s1 }}
             <EInput v-model="s1"></EInput>
+            {{  model.s1 }} - {{ obj.s2.value }}
         </div>
 
         <hr />
