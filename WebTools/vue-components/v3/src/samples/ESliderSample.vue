7407f37738719bf34ed23b438f9cb433414c848b
sigeer <1452069418@qq.com>
2023-12-05 14:26:08 +0800

    feat: 滑动输入条

---
+++ WebTools/vue-components/v3/src/samples/ESliderSample.vue
@@ -0,0 +1,14 @@
+<template>
+    <Slider v-model:rate="rate" style="margin-bottom: 24px;"></Slider>
+    <RangeSlider v-model:range="range"></RangeSlider>
+    {{ rate }} - {{ range }}
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Slider from '../components/slider';
+import RangeSlider from '../components/range-slider/RangeSlider.vue';
+
+const rate = ref(20)
+const range = ref([0, 30]);
+</script>
